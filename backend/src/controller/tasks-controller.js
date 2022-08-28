b1fc289342400d42fefdd94e1507fc2d6548c1ff
Rafael <rafaelberger.dev@gmailcom>
2022-08-27 23:19:00 -0300

    query para buscar dinheiro de despesas/receitas e roteado

--- backend/src/controller/tasks-controller.js
+++ backend/src/controller/tasks-controller.js
@@ -8,3 +8,24 @@ exports.GetTasksData = async (_req, res) => {
     console.log(error);
   }
 };
+
+exports.GetBalanceData = async (_req, res) => {
+  try {
+    const income = await Tasks.findAll({
+      attributes: ["money"],
+      where: {
+        positive: true
+      }
+    })
+    const expense = await Tasks.findAll({
+      attributes: ["money"],
+      where: {
+        positive: false
+      }
+    })
+    
+    res.send({renda: income, gasto: expense })
+  } catch (error) {
+    console.log(error)
+  }
+}
--- backend/src/router/tasksRouter.js
+++ backend/src/router/tasksRouter.js
@@ -1,7 +1,8 @@
-const { GetTasksData } = require("../controller/tasks-controller");
+const { GetTasksData, GetBalanceData } = require("../controller/tasks-controller");
 const express = require("express");
 const taskRouters = express.Router();
 
 taskRouters.get("/tasks", GetTasksData);
+taskRouters.get("/balance", GetBalanceData);
 
 module.exports = taskRouters;
