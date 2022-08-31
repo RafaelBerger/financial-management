80005dc265d3191e78532b787570ecf39117b601
Rafael <rafaelberger.dev@gmailcom>
2022-08-30 23:07:18 -0300

    API criado para criar e excluir tasks

--- frontend/src/api/taskApi.tsx
+++ frontend/src/api/taskApi.tsx
@@ -1,14 +1,45 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "http://localhost:8080/";
 
 export const getAllTasks = async () => {
-  const response = await axios.get(`${BACKEND_URL}/tasks`);
+  const response = await axios.get(`${BACKEND_URL}tasks`);
   return response.data;
 };
 
 export const getBalance = async () => {
-  const response = await axios.get(`${BACKEND_URL}/balance`)
-  return response.data
+  const response = await axios.get(`${BACKEND_URL}balance`);
+  return response.data;
+};
+
+interface postArgs {
+  descricao: string;
+  dinheiro: number;
+  positivo: boolean;
+  data_registro: string;
 }
 
+export const postTasks = async (
+  descricao: postArgs,
+  dinheiro: postArgs,
+  positivo: postArgs,
+  data: postArgs
+) => {
+  const response = await axios.post(BACKEND_URL, {
+    descriptions: descricao,
+    money: dinheiro,
+    positive: positivo,
+    data_registro: data,
+  });
+
+  return response;
+};
+
+interface deleteTaskArgs {
+  id: number;
+}
+
+export const deleteTask = async (id: deleteTaskArgs) => {
+  const response = await axios.delete(BACKEND_URL + `${id}`);
+  return response;
+};
