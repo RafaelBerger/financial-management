c57a3316da78292f08b4b36940381cafba504a30
Rafael <rafaelberger.dev@gmailcom>
2022-08-30 23:07:58 -0300

    implementado a feature de salvar tasks

--- frontend/src/components/ModalCreate.tsx
+++ frontend/src/components/ModalCreate.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { postTasks } from "../api/taskApi";
 
 const ModalCreate = () => {
   const [inputText, setInputText] = useState("");
@@ -8,57 +9,83 @@ const ModalCreate = () => {
 
   function handleInputText(e: any) {
     setInputText(e.target.value);
-    console.log(inputText);
   }
 
   function handleInputNumber(e: any) {
     setInputNumber(e.target.value);
-    console.log(inputNumber);
   }
 
   function handleInputRadio(e: any) {
     setInputRadio(e.target.value);
-    console.log(inputRadio);
   }
 
   function handleInputDate(e: any) {
     setInputDate(e.target.value);
-    console.log(inputDate);
   }
+  //#region
+  const dateCalendar = new Date();
+  const getMonth = dateCalendar.getMonth() + 1;
+  const monthFormated = getMonth < 10 ? `0${getMonth}` : `${getMonth}`;
+  const year = dateCalendar.getFullYear();
+  //#endregion
 
-  const dateCalendar = new Date()
-  const getMonth = dateCalendar.getMonth() + 1
-  const monthFormated = getMonth < 10 ? `0${getMonth}` : `${getMonth}`
-  const year = dateCalendar.getFullYear()
-  
+  const saveNewTask = async () => {
+    if (
+      inputText === "" ||
+      inputNumber === "" ||
+      inputRadio == "" ||
+      inputDate == ""
+    ) {
+      return "";
+    } else {
+      await postTasks(inputText, inputNumber, inputRadio, inputDate);
+
+      setInputDate("");
+      setInputText("");
+      setInputRadio("");
+      setInputNumber("");
+    }
+  };
+
   return (
     <>
       <h1 className="flex justify-center mt-8 mb-4">
         Informe os dados necessários para criar um card
       </h1>
       <form className="flex flex-col w-full h-3/5 justify-around items-center gap-4">
-        <input type="month" max={`${year}-${monthFormated}`} className="text-black w-2/4 p-2 rounded-md" onChange={handleInputDate}/>
+        <input
+          type="month"
+          max={`${year}-${monthFormated}`}
+          className="text-black w-2/4 p-2 rounded-md"
+          onChange={handleInputDate}
+          required
+        />
         <input
           type="text"
           placeholder="Descrição"
           className="text-black w-2/4 p-2 rounded-md "
           onChange={handleInputText}
+          required
         />
         <input
           type="number"
           placeholder="Valor"
           className="text-black w-2/4 p-2 rounded-md"
           onChange={handleInputNumber}
+          required
         />
-        <div className="flex w-full justify-center gap-8" onChange={handleInputRadio}>
-          <div className="flex flex-col gap-4" >
+        <div
+          className="flex w-full justify-center gap-8"
+          onChange={handleInputRadio}
+        >
+          <div className="flex flex-col gap-4">
             <input
               type="radio"
               id="ganho"
               name="receita"
-              value="ganho"
+              value="true"
               className="text-black scale-[2]"
-              
+              required
             />
             <label htmlFor="ganho">Ganho</label>
           </div>
@@ -67,8 +94,9 @@ const ModalCreate = () => {
               type="radio"
               id="gasto"
               name="receita"
-              value="gasto"
+              value="false"
               className="text-black scale-[2]"
+              required
             />
             <label htmlFor="gasto">Gasto</label>
           </div>
@@ -76,10 +104,8 @@ const ModalCreate = () => {
         <button
           type="submit"
           className="w-auto py-2 px-10 rounded-lg bg-sky-500 hover:bg-sky-700 transition-colors"
-          onClick={(e) => {
-            e.preventDefault()
-            console.log(inputText, inputRadio, inputNumber, inputDate);
-            
+          onClick={() => {
+            saveNewTask();
           }}
         >
           Salvar
