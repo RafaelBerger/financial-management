9e6452f5f7321369e9ac9aa21cc6427e69261a7c
Rafael <rafaelberger.dev@gmailcom>
2022-09-15 20:28:40 -0300

    arquivo de api desnecessário

--- frontend/src/api/informationBarApi.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BACKEND_URL = "http://localhost:8080/";
-
-export const getExpense = () => {
-  const response = axios.get(`${BACKEND_URL}expense`);
-  return response;
-};
